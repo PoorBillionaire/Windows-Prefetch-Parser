319d62d3b3d687d447e8d64689960a2f68848d3b
Adam Witt <accidentalassist@gmail.com>
2016-01-25 10:19:03 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='windowsprefetch',
-    version='2.0.0',
+    version='2.0.3',
     description='A Python script to parse Windows Prefetch files',
     long_description=long_description,
     url='https://github.com/PoorBillionaire/Windows-Prefetch-Parser',
